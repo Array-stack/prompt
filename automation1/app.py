2c122346997194c167e2eb69a1de05ebb0e9e43a
Hicham Azaroual <113389908+Array-stack@users.noreply.github.com>
2023-05-21 21:41:01 +0200

    Update app.py

--- automation1/app.py
+++ automation1/app.py
@@ -1,71 +1,54 @@
+import requests
 from flask import Flask, render_template, request
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
-import sys
-
-
+import openai
 
 app = Flask(__name__)
-
 # Beispielwerte für die Ergebnisse und ausgewählten Optionen
-titles = ['Option 1', 'Option 2', 'Option 3']
-links = ['#', '#', '#']
+titles = []
+links = []
 selected_options = []
 
 # Benutzerdefinierte Funktion zum Kombinieren von zwei Listen
 def combine_lists(a, b):
     return zip(a, b)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def search():
+    global selected_options
+
     if request.method == 'POST':
         search_term = request.form["search_term"]
         selected_options = request.form.getlist('selected_options')
+        print(request.form)
 
-    # Beispiel: Drucken Sie den Suchbegriff und die ausgewählten Optionen
-        print("Suchbegriff:", search_term)
-        print("Ausgewählte Optionen:", selected_options)
-        sys.stdout.flush()
-
-
-
-        # Pfad zum ChromeDriver
-        driver_path = "./chromedriver_win32/chromedriver.exe"
-    
-        # ChromeDriver-Service erstellen
+        driver_path = "./chromedriver"
         service = Service(driver_path)
-    
-        # Starte den Webdriver
         driver = webdriver.Chrome(service=service)
-    
-        # Navigiere zur Google-Suche
+
         driver.get("https://www.google.com")
-    
-        # Akzeptiere die Cookies, falls das Element vorhanden ist
         try:
-            accept_cookies_button = driver.find_element("id", "L2AGLb")
+            accept_cookies_button = driver.find_element(By.ID, "L2AGLb")
             accept_cookies_button.click()
         except:
             pass
-    
+
         time.sleep(2)
-    
-        # Suche nach dem Suchfeld und gib den Suchbegriff ein
-        search_box = driver.find_element("name", "q")
+
+        search_box = driver.find_element(By.NAME, "q")
         search_box.send_keys(search_term)
         search_box.send_keys(Keys.RETURN)
-    
+
         time.sleep(2)
-    
-        # Extrahiere die Suchergebnisse
+
         soup = BeautifulSoup(driver.page_source, "html.parser")
         search_results = soup.select('div.g')
-    
-        # Extrahiere die ersten 5 Suchergebnisse
+
         titles = []
         links = []
         for result in search_results[:3]:
@@ -75,19 +58,40 @@ def search():
                 link = result.find('a')['href']
                 titles.append(title)
                 links.append(link)
-    
+
         time.sleep(2)
-    
-        # Schließe den Webdriver
+
         driver.quit()
-    
+
         return render_template('index.html', search_term=search_term, titles=titles, links=links, combine_lists=combine_lists, selected_options=selected_options)
-    
+
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/send_prompt', methods=['POST'])
+def send_prompt():
+    search_term = request.form["search_term"]
+    selected_options = request.form.getlist('selected_options')
 
+    # Define OpenAI API key 
+    openai.api_key = "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"
+
+    # Set up the model and prompt
+    model_engine = "text-davinci-003"
+    prompt = "Bitte lesen Sie die ausgewählten Links sorgfältig durch und generieren Sie einen Titel sowie einen Text von 200 Wörtern basierend auf den Informationen in den Links." + search_term + str(selected_options)
+
+    # Generate a response
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    response = completion.choices[0].text
+
+    return render_template('index.html', search_term=search_term, titles=titles, links=links, combine_lists=combine_lists, selected_options=selected_options, response=response)
 
 
 
